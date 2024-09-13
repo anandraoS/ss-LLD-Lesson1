b566e0869a26e6e729c10af196ebe9414f2b163f
anandaraosahukari <sahukari.anandarao@gmail.com>
2024-09-13 09:10:45 +0530

    Lesson 5 ISP
    Interface Seggregation Principle

---
+++ src/main/java/Module04/BuilderDesignPattern/Product.java
@@ -0,0 +1,31 @@
+package Module04.BuilderDesignPattern;
+
+import java.util.List;
+
+public class Product {
+    private String name;
+    private String desc;
+    private int price;
+    private String brand;
+    private String category;
+    private int discount;
+    private String createdAt;
+    private String updatedAt;
+    private List<String> images;
+
+    public Product() {
+    }
+
+    public Product(String name, String desc, int price, String brand, String category, int discount, String createdAt, String updatedAt, List<String> images) {
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.brand = brand;
+        this.category = category;
+        this.discount = discount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.images = images;
+    }
+    
+}
